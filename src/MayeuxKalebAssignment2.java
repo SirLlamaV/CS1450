4be220ba6ebc628311f2d9787b43f45444d0cc04
SirLlamaV <82250652+SirLlamaV@users.noreply.github.com>
2021-09-06 16:42:40 -0600

    OOP Review

--- src/MayeuxKalebAssignment2.java
+++ src/MayeuxKalebAssignment2.java
@@ -1,4 +1,28 @@
+import java.io.IOException;
+import java.io.File;
+import java.util.Scanner;
+
 public class MayeuxKalebAssignment2 
 {
+	public static void main(String[] args) throws IOException
+	{
+		File fileName = new File("Actors.txt");
+		Scanner inputFile = new Scanner (fileName);
+		
+		while(inputFile.hasNextLine())
+		{
+			System.out.println("Value read form file is: " + inputFile.nextLine());
+		}
+		
+		inputFile.close();
+	}
+	
+}
 
+class Actor
+{
+	private String Hero;
+	private String Villain;
+	private String Monster;
+	private String Droid;
 }
