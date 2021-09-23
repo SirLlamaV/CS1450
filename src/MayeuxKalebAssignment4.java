98c715d0aaab2fa5ef2796d136bee7fab87b87f3
SirLlamaV <82250652+SirLlamaV@users.noreply.github.com>
2021-09-23 12:38:11 -0600

    Beginning of Code
    9-21-21

--- src/MayeuxKalebAssignment4.java
+++ src/MayeuxKalebAssignment4.java
@@ -1,10 +1,24 @@
+/*
+ * Initial Comment 
+ */
+
+import java.io.IOException;
+import java.io.File;
+import java.util.Scanner;
+import java.util.ArrayList;
 
 public class MayeuxKalebAssignment4 
 {
 
-	public static void main(String[] args) 
+	public static void main(String[] args) throws IOException
 	{
-
+		File fileName = new File("Trains.txt");
+		Scanner inputFile = new Scanner(fileName);
+		
+		
+		
+		
+		inputFile.close();
 	}
 
 }
