56d5043f641c9ebeba3e35e6ff143e89e8709533
SirLlamaV <82250652+SirLlamaV@users.noreply.github.com>
2021-09-28 13:12:22 -0600

    Bugging Out

--- src/MayeuxKalebAssignment4.java
+++ src/MayeuxKalebAssignment4.java
@@ -14,7 +14,29 @@ public class MayeuxKalebAssignment4
 	{
 		File fileName = new File("Trains.txt");
 		Scanner inputFile = new Scanner(fileName);
+		int fileSize = inputFile.nextInt();
 		
+		int trackNumber;
+		int engineNumber;
+		String company;
+		int numberRailCars;
+		String type;
+		String destinationCity;
+		
+		Railroad railroad = new Railroad(fileSize);
+		
+		for (int i = 0; i < fileSize; i++)
+		{
+			trackNumber = inputFile.nextInt();
+			engineNumber = inputFile.nextInt();
+			company = inputFile.next();
+			numberRailCars = inputFile.nextInt();
+			type = inputFile.next();
+			destinationCity = inputFile.next();
+			
+			Train train = new Train(engineNumber, company, numberRailCars, type, destinationCity);
+			train.getEngineNumber();
+		}
 		
 		
 		
@@ -22,3 +44,101 @@ public class MayeuxKalebAssignment4
 	}
 
 }
+
+class Railroad
+{
+	private int numberTracks;
+	private Train[] sortingYard;
+	
+	public Railroad (int numberTracks)
+	{
+		sortingYard = new Train[numberTracks];
+	}
+	
+	public int getNumberTracks(int numberTracks)
+	{
+		return numberTracks;
+	}
+	
+	public void addTrainToSortingYard(int trackNumber,Train train)
+	{
+		sortingYard[trackNumber] = train;
+	}
+	
+	public Train getTrainInSortingYard(int trackNumber)
+	{
+		return sortingYard[trackNumber];
+	}
+	
+	public void displaySortingYard()
+	{
+		
+	}
+}
+
+class Train implements Comparable<Train>
+{
+	private int engineNumber;
+	private String company;
+	private int numberRailCars;
+	private String type;
+	private String destinationCity;
+	
+	public Train (int engineNumber, String company, int numberRailCars, String type, String destinationCity)
+	{
+		this.engineNumber = engineNumber;
+		this.company = company;
+		this.numberRailCars = numberRailCars;
+		this.type = type;
+		this.destinationCity = destinationCity;
+	}
+	
+	public int getEngineNumber()
+	{
+		return engineNumber;
+	}
+	
+	public String getCompany()
+	{
+		return company;
+	}
+	
+	public int getNumberRailCars()
+	{
+		return numberRailCars;
+	}
+	
+	public String getType()
+	{
+		return type;
+	}
+	
+	public String getDestinationCity()
+	{
+		return destinationCity;
+	}
+	
+	@Override
+	public String toString()
+	{
+		return String.format("%d\\t%s\\t\\t%d\\t\\t%-15s\\t%s", engineNumber, company, numberRailCars, type, destinationCity);
+	}
+	
+	@Override
+	public int compareTo(Train otherTrain)
+	{
+		if (this.numberRailCars > otherTrain.numberRailCars)
+		{
+			return 1;
+		}
+		else if (this.numberRailCars < otherTrain.numberRailCars)
+		{
+			return -1;
+		}
+		else
+		{
+			return 0;
+		}
+	}
+}
+
